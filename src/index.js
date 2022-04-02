e7fc870444c0b9f73a9199702331c6606b995d35
marmooo <marmooo@users.noreply.github.com>
2022-04-02 14:35:43 +0900

    remove grade=2

--- src/index.js
+++ src/index.js
@@ -143,7 +143,7 @@ function speak(text) {
 }
 
 function loadProblems() {
-  const grade = gradeOption.selectedIndex + 2;
+  const grade = gradeOption.selectedIndex + 3;
   const course = document.getElementById("course").textContent;
   const dir = (course == "短文") ? "easy/" : "hard/";
   if (grade > 0) {
