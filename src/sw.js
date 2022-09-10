2cd5b98ab8084ccbe65a385070aee00abbf4921d
marmooo <marmooo@users.noreply.github.com>
2022-09-11 08:54:59 +0900

    bump bootstrap from 5.2.0 to 5.2.1

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-07-24 00:46";
+var CACHE_NAME = "2022-09-11 08:49";
 var urlsToCache = [
   "/speecha/",
   "/speecha/index.js",
@@ -10,7 +10,7 @@ var urlsToCache = [
   "/speecha/mp3/keyboard.mp3",
   "/speecha/voice.svg",
   "/speecha/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css",
 ];
 
 self.addEventListener("install", function (event) {
