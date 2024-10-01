02e13ba9e2e216505aede8e830ceb2bcb22543ae
marmooo <marmooo@users.noreply.github.com>
2024-10-01 09:27:02 +0900

    use TextLineStream

--- build.js
+++ build.js
@@ -1,13 +1,15 @@
-import { readLines } from "https://deno.land/std/io/mod.ts";
+import { TextLineStream } from "jsr:@std/streams/text-line-stream";
 
 async function build(threshold) {
   const dict = new Map();
   const alphabets = "abcdefghijklmnopqrstuvwxyz".split("");
   for (const alphabet of alphabets) {
     const fileName = `google-ngram-small-en/dist/1gram/${alphabet}.csv`;
-    const fileReader = await Deno.open(fileName);
-    for await (const line of readLines(fileReader)) {
-      if (!line) continue;
+    const file = await Deno.open(fileName);
+    const lineStream = file.readable
+      .pipeThrough(new TextDecoderStream())
+      .pipeThrough(new TextLineStream());
+    for await (const line of lineStream) {
       const arr = line.split(",");
       const lemma = arr[0];
       if (!/^[a-z]+$/.test(lemma)) continue;
@@ -17,7 +19,7 @@ async function build(threshold) {
   }
   const arr = Array.from(dict);
   arr.sort((a, b) => b[1] - a[1]);
-  const result = arr.slice(0, threshold).map(x => x[0]).join("\n");
+  const result = arr.slice(0, threshold).map((x) => x[0]).join("\n");
   Deno.writeTextFileSync(`src/words.lst`, result);
 }
 
